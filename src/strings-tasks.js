d948e627da30a44ee1fbd3274f204381a70c955d
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-14 18:49:01 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -37,8 +37,12 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+    if (typeof value === 'string'|| value instanceof String ){
+    return true;
+  } else {
+    return fasle;
+  }
 }
 
 /**
