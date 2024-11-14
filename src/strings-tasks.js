d77864d708da086c4d68249c19757a5a83064014
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-14 19:13:52 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -305,8 +305,9 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  return str == str.split('').reverse().join('');
 }
 
 /**
