45b892b95d3e9617c4a2734198acb2c26a93965b
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-15 23:39:00 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -178,8 +178,14 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+const str1 = str.split('');
+let rez = 0 ;
+  for (let i = 0; i < str.length; i+=1){
+    str1[i] = str[i].charCodeAt(0);
+    rez += str[i];
+  }
+ return rez;
 }
 
 /**
