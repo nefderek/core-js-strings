ef11d522c3b27949a622720f68a8c91ba6c556f7
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-20 01:32:45 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -134,8 +134,12 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  const rez = str.repeat(times);
+  if (times < 0) {
+    return '';
+  }
+  return rez;
 }
 
 /**
