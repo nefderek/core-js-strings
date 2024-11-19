7af1ed7dc44bae9a055c36b1eb5297af4caa600e
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-20 01:29:32 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -68,8 +68,12 @@ function concatenateStrings(value1, value2) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  const val1 = value[0];
+  if (value === '') {
+    return '';
+  }
+  return val1;
 }
 
 /**
