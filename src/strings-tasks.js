62fe18414a498ad334b1ae4fcb9b6109e5314229
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-21 22:59:29 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -20,7 +20,7 @@
  *   getStringLength(undefined) => 0
  */
 function getStringLength(value) {
-  if (value < 1) {
+  if (value === null || value === undefined || value < 1) {
     return 0;
   }
   return value.length;
