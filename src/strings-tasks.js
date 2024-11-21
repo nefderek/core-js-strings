aa6349ae6030d57df97ee72b46a15dd3598f6d95
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-21 23:25:51 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -309,8 +309,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  let count = 0;
+  const arr = 'aeiouyAEIOUY';
+  for (let i = 0; i < str.length; i += 1) {
+    if (arr.includes(str[i])) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
