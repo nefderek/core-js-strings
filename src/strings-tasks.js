fef68f0850e2b22220cbb8baac93d9f10a8b83d0
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-21 23:42:10 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -383,7 +383,6 @@ function invertCase(/* str */) {
 }
 
 
-
 /**
  * Returns the result of string template and given parameters firstName and lastName.
  * Please do not use concatenation, use template string :
