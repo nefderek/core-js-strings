acea7fa943fbdc203fecb6033edcb01bdf06aab8
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-21 23:40:30 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -381,8 +381,8 @@ function reverseWords(/* str */) {
 function invertCase(str) {
   let newStr = "";
   for (let i = 0; i < str.length; i += 1) {
-    if (str[i] === str[i].toLowerCase()) {
-      newStr += str[i].toUpperCase();
+  if (str[i] === str[i].toLowerCase()) {
+    newStr += str[i].toUpperCase();
     }
     newStr += str[i].toLowerCase();
   }
