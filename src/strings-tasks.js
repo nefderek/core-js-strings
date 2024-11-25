7d24cc89125230f3aeb4027041a0a49ed311efa9
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-26 01:08:33 +0700

    fix: Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -411,8 +411,13 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  let hi = value.replace('Hello,', '');
+  let hi2 = hi.replace('!', '').trimStart();
+  let hi3 = hi2.split(' ');
+  let firstName = hi3[0];
+  let lastName = hi3[1];
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
